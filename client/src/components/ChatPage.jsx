4818dee213c79b62196d7e9b87f54d3587fa3011
Tanmay Kanase <tanmaykanase06@gmial.com>
2024-12-26 15:13:32 +0530

    Final UI created

--- client/src/components/ChatPage.jsx
+++ client/src/components/ChatPage.jsx
@@ -1,19 +1,20 @@
-import { MdSend } from "react-icons/md";
+import { useRef, useState } from "react";
+import { MdAttachFile, MdSend } from "react-icons/md";
 
 const ChatPage = () => {
   return (
     <div className="">
-      <header className="dark:border-gray-700 dark:bg-gray-900  py-5 shadow  flex justify-around w-full fixed h-20">
+      <header className="dark:border-gray-700 fixed w-full dark:bg-gray-900  py-5 shadow  flex justify-around w-full fixed h-20">
         {/* Room name container*/}
         <div>
           <h1 className="text-2xl font-semibold">
-            Room : <span>Fami ROom </span>
+            Room : <span>Fami Room </span>
           </h1>
         </div>
         {/* Username container */}
         <div>
           <h1 className="text-2xl font-semibold">
-            Room : <span>Fami ROom </span>
+            Room : <span>Fami Room </span>
           </h1>
         </div>
         {/* Leave Room */}
@@ -24,13 +25,25 @@ const ChatPage = () => {
         </div>
       </header>
 
+      <main className="py-22  h-screen w-2/3 dark:bg-slate-600 mx-auto overflow-auto ">
+        <div className="message_container"></div>
+      </main>
       {/* Input message container */}
       <div className="fixed bottom-2 w-full h-16">
         <div className="h-full px-2 gap-4  flex items-center justify-between rounded w-2/3 mx-auto dark:bg-gray-900 ">
-          <input type="text" placeholder="Type your message here..." className="dark:bg-gray-700 w-full dark:border-gray-700 px-3 py-2 rounded-full focus:outline-none h-full focus:ring-0 " />
-          <button className="dark:bg-green-500 h-10 w-10  flex justify-center items-center  rounded-full">
-            <MdSend size={20} />
-          </button>
+          <input
+            type="text"
+            placeholder="Type your message here..."
+            className="dark:bg-gray-700 w-full dark:border-gray-700 px-3 py-2 rounded-full focus:outline-none h-full focus:ring-0 "
+          />
+          <div className="flex gap-1 ">
+            <button className="dark:bg-purple-500 h-10 w-10  flex justify-center items-center  rounded-full">
+              <MdAttachFile size={20} />
+            </button>
+            <button className="dark:bg-green-500 h-10 w-10  flex justify-center items-center  rounded-full">
+              <MdSend size={20} />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- client/src/components/JoinRoom.jsx
+++ client/src/components/JoinRoom.jsx
@@ -1,23 +1,33 @@
-import chaticon from "../assets/chat.png"
-
+import chaticon from "../assets/chat.png";
 
 const JoinRoom = () => {
   return (
     <div className="min-h-screen flex items-center justify-center ">
       <div className="p-10 dark:border-gray-700 w-full flex flex-col gap-2 max-w-md rounded-none bg-gray-900">
-        <div><img src={chaticon} className="w-24 mx-auto" />
+        <div>
+          <img src={chaticon} className="w-24 mx-auto" />
         </div>
         <h1 className="text-2xl font-semibold text-center">
           Join room / create room
         </h1>
         <div>
           <label htmlFor="" className="block font-medium mb-2 ">
-            Your name 
+            Your name
           </label>
           <input
             type="text"
             id="name"
-            className="w-full dark:bg-gray-600 px-4 py-2 border dark:border-gray-300 rounded"
+            className="w-full dark:bg-gray-600 px-4 py-2 border dark:border-gray-300 rounded-full"
+          />
+        </div>
+        <div>
+          <label htmlFor="" className="block font-medium mb-2 ">
+            Room Id / New Room Id
+          </label>
+          <input
+            type="text"
+            id="name"
+            className="w-full dark:bg-gray-600 px-4 py-2 border dark:border-gray-300 rounded-full"
           />
         </div>
         {/* Button */}
